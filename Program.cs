2c1aae8695f0a3735b73d16efae090691a34882b
Asfahan Tutorial <asfahan.dotnet.tutorials@gmail.com>
2026-03-09 00:41:22 +0530

    Program file updated

--- Program.cs
+++ Program.cs
@@ -3,12 +3,18 @@ using StudentManagement.API.Services;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddControllers()
+.AddJsonOptions(options =>
+{
+    // Keep property names exactly as defined in C# model classes (NO camelCase)
+    options.JsonSerializerOptions.PropertyNamingPolicy = null;
+});
 
-builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
 builder.Services.AddSingleton<IStudentService, StudentService>();
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
